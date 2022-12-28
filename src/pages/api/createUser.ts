e279ebcac8b3b4a4ab85c6237834ac96e94ba9ac
ParthMmm <parthmangrola@gmail.com>
2022-12-27 20:37:21 -0800

    updated webhook

--- src/pages/api/createUser.ts
+++ src/pages/api/createUser.ts
@@ -8,9 +8,17 @@ export default async function handle(
   res: NextApiResponse
 ) {
   console.log(req);
-  return res.json({ message: "Hello World" });
-  //   const posts = await prisma.user.create({
-  //     data: {
-  //         id: id;
-  //   res.json(posts);
+
+  const { id, username } = req.body.data;
+
+  const user = await prisma.user.create({
+    data: {
+      id: id,
+      username: username,
+    },
+  });
+
+  if (user) {
+    res.status(200).json({ message: "User created", user });
+  }
 }
