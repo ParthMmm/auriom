8120d90ee340edf1fd8d00d34a1c1518ed790128
ParthMmm <parthmangrola@gmail.com>
2023-01-10 11:56:15 -0800

    updated webhooks

--- src/pages/api/updateUser.ts
+++ src/pages/api/updateUser.ts
@@ -7,34 +7,41 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { id, username, external_accounts, profile_image_url } = req.body.data;
-  let img = '';
+  try {
+    const { id, username, external_accounts, profile_image_url } =
+      req.body.data;
+    let img = '';
 
-  if (external_accounts && external_accounts.length > 0) {
-    const externalAccount = external_accounts[0];
-    const externalImg = externalAccount.picture;
+    if (external_accounts && external_accounts.length > 0) {
+      const externalAccount = external_accounts[0];
+      const externalImg = externalAccount.picture;
 
-    if (profile_image_url === 'https://www.gravatar.com/avatar?d=mp') {
-      img = externalImg;
+      if (profile_image_url === 'https://www.gravatar.com/avatar?d=mp') {
+        img = externalImg;
+      } else {
+        img = profile_image_url;
+      }
     } else {
       img = profile_image_url;
     }
-  } else {
-    img = profile_image_url;
-  }
 
-  const user = await prisma.user.update({
-    where: {
-      id: id,
-    },
-    data: {
-      profileImage: img,
-      username: username,
-    },
-  });
+    const user = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        profileImage: img,
+        username: username,
+      },
+    });
 
-  if (user) {
-    res.status(200).json({ message: 'User updated', user });
+    if (user) {
+      res.status(200).json({ message: 'User updated', user });
+    }
+
+    throw new Error('User not updated');
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
-  return res.status(400).json({ message: 'User not updated' });
+  // return res.status(400).json({ message: 'User not updated' });
 }
