7dd4657001ce5d77bfe9cca2f5e2e13ac8e31a0c
ParthMmm <parthmangrola@gmail.com>
2023-01-10 12:13:17 -0800

    bug: fixing webhooks

--- src/pages/api/createUser.ts
+++ src/pages/api/createUser.ts
@@ -40,6 +40,7 @@ export default async function handle(
       res.status(200).json({ message: 'User created', user });
       return;
     }
+    throw new Error('User not created');
   } catch (err) {
     res.status(500).json({ message: err });
   }
--- src/pages/api/updateUser.ts
+++ src/pages/api/updateUser.ts
@@ -12,6 +12,8 @@ export default async function handle(
       req.body.data;
     let img = '';
 
+    console.log(id, username, external_accounts, profile_image_url);
+
     if (external_accounts && external_accounts.length > 0) {
       const externalAccount = external_accounts[0];
       const externalImg = externalAccount.picture;
@@ -25,24 +27,25 @@ export default async function handle(
       img = profile_image_url;
     }
 
-    await prisma.user
-      .update({
-        where: {
-          id: id,
-        },
-        data: {
-          profileImage: img,
-          username: username,
-        },
-      })
-      .then((user) => {
-        res.status(200).json({ message: 'User updated', user });
-      })
-      .catch(() => {
-        res.status(500).json({ message: 'user not updated' });
-      });
+    const user = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        profileImage: img,
+        username: username,
+      },
+    });
+
+    if (user) {
+      res.status(200).json({ message: 'User updated', user });
+      return;
+    }
+
+    throw new Error('User not updated');
   } catch (err) {
     res.status(500).json({ message: err });
   }
-  // return res.status(400).json({ message: 'User not updated' });
+
+  return;
 }
