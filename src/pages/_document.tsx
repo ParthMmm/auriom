842005b7769ce67b869bfae25b3b30b856842e72
ParthMmm <parthmangrola@gmail.com>
2023-01-27 18:26:59 -0800

    style: update og image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+const imageURL = 'https://www.auriom.club/api/og';
+
 export default function Document() {
   return (
     <Html>
@@ -14,6 +16,17 @@ export default function Document() {
           content="The social platform for music lovers."
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content={imageURL} />
+        <meta property="og:url" content="https://auriom.club" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="auriom.club" />
+        <meta name="twitter:card" content={imageURL} />
+        <meta name="twitter:image" content={imageURL} />
+        <meta name="twitter:title" content="auriom.club" />
+        <meta
+          name="twitter:description"
+          content="The social platform for music lovers."
+        />
       </Head>
       <body className="bg-black">
         <Main />
